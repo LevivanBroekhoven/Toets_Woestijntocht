d4216c27fbc4f73df353c6c5408988e5aa30e89b
LevivanBroekhoven <99076037@mydavinci.nl>
2024-06-21 09:22:05 +0200

    Opdraccht 1 af

---
+++ Opdracht1.js
@@ -0,0 +1,16 @@
+function kanBereikenTankstation(kilometersNaarTankstation, kilometersPerLiter, resterendeLiters) {
+    test = resterendeLiters * kilometersPerLiter 
+    if (test >= kilometersNaarTankstation)
+    return true;
+
+    else
+    return false;
+    
+}
+
+log1 = console.log(kanBereikenTankstation(80, 10, 8));
+log2 = console.log(kanBereikenTankstation(90, 10, 8));
+log3 = console.log(kanBereikenTankstation(80, 20, 4));
+log4 = console.log(kanBereikenTankstation(150, 15, 10));
+log5 = console.log(kanBereikenTankstation(40, 10, 5));
+log6 = console.log(kanBereikenTankstation(100, 10, 9));
